289eccfea0e6570f0c31bd1531cbf23d1d32ccf1
kajal764 <kajaldw666@gmail.com>
2019-12-24 12:57:41 +0530

    Added: Test Cases To Validate Last Name

--- test/java/UserRegisterationTest.java
+++ test/java/UserRegisterationTest.java
@@ -3,27 +3,56 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegisterationTest {
+    //Test cases to validate first name
     @Test
     public void whenGivenFirstName_ShouldHaveMinimum3CharWithCamelCase(){
         UserRegisteration firstName = new UserRegisteration();
-        String fname = firstName.checkFirstName("Kajal");
+        String fname = firstName.checkName("Kajal");
         Assert.assertEquals("Valid",fname);
     }
     @Test
     public void whenGivenFirstName_WithSmallLetter(){
         UserRegisteration firstName= new UserRegisteration();
-        String fname = firstName.checkFirstName("kajal");
+        String fname = firstName.checkName("kajal");
         Assert.assertEquals("Invalid",fname);
     }
+    @Test
     public void whenGivenFirstName_With2Character(){
         UserRegisteration firstName= new UserRegisteration();
-        String fname = firstName.checkFirstName("ka");
+        String fname = firstName.checkName("ka");
         Assert.assertEquals("Invalid",fname);
     }
+    @Test
     public void whenGivenFirstName_WithOtherLetterCapital(){
         UserRegisteration firstName= new UserRegisteration();
-        String fname = firstName.checkFirstName("kAjal");
+        String fname = firstName.checkName("kAjal");
         Assert.assertEquals("Invalid",fname);
+    }
+
+    //Test case to validate last name
+    @Test
+    public void whenGivenLastName_ShouldHaveMinimum3CharWithCamelCase(){
+        UserRegisteration lastName = new UserRegisteration();
+        String lname = lastName.checkName("Waghmare");
+        Assert.assertEquals("Valid",lname);
+    }
+    @Test
+    public void whenGivenLastName_WithSmallLetter(){
+        UserRegisteration lastName= new UserRegisteration();
+        String lname = lastName.checkName("waghmare");
+        Assert.assertEquals("Invalid",lname);
+    }
+    @Test
+    public void whenGivenLastName_With2Character(){
+        UserRegisteration lastName= new UserRegisteration();
+        String lname = lastName.checkName("ma");
+        Assert.assertEquals("Invalid",lname);
+    }
+    @Test
+    public void whenGivenLastName_WithOtherLetterCapital(){
+        UserRegisteration lastName= new UserRegisteration();
+        String lname = lastName.checkName("wagHmare");
+        Assert.assertEquals("Invalid",lname);
     }
 
 }
--- test/java/com/bridgelabz/UserRegisteration.java
+++ test/java/com/bridgelabz/UserRegisteration.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 
 public class UserRegisteration {
 
-    public String checkFirstName(String fname) {
-        if(fname.matches("^[A-Z]{1}[a-z]{2,}$"))
+    public String checkName(String name) {
+        if(name.matches("^[A-Z]{1}[a-z]{2,}$"))
             return "Valid";
         return "Invalid";
     }
